917b5e08c72ce8572d517274ba395c4a908c1307
sophia <sophiaciji@gmail.com>
2024-09-26 20:45:16 -0500

    added auto strafing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoStrafingCode.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="autoStrafingCode", group="auto")
+public class autoStrafingCode extends LinearOpMode {
+
+    DcMotor leftWheel;
+    DcMotor rightWheel;
+
+    @Override
+    public void runOpMode () throws} InterruptedException {
+        leftWheel = hardwareMap.dcMotor.get("left_wheel");
+        rightWheel = hardwareMap.dcMotor.get("right_wheel");
+        leftWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        leftWheel.setPower();
+        rightWheel.setPower();
+
+        sleep( milliseconds: 1000);
+
+        leftWheel,setPower();
+        rightWheel.setPower();
+
+        leftWheel.setPower();
+        rightWheel.setPower();
+
+        sleep(milliseconds: 500);
+
+        leftWheel,setPower();
+        rightWheel.setPower();
+
+        leftWheel,setPower();
+        rightWheel.setPower();
+
+        sleep(milliseconds: 700);
+
+        leftWheel.setPower();
+        rightWheel.setPower();
+
+    }
+}
