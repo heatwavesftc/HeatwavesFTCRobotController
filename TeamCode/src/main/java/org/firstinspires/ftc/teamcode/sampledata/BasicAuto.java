af2cf8d1130fa6061869794da961922014870201
Richard Sebastian <Richardchurchil2010@gmail.com>
2024-10-22 20:17:51 -0500

    Auto Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampledata/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sampledata/BasicAuto.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.sampledata;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@Autonomous
 public class BasicAuto extends LinearOpMode {
 
     //Motors
@@ -20,29 +22,48 @@ public class BasicAuto extends LinearOpMode {
         motorLeftBack = hardwareMap.get(DcMotor.class, "BL");
         motorLeftFront = hardwareMap.get(DcMotor.class, "FL");
 
+        motorRightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
         motorRightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         motorRightFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-        while(opModeIsActive()){
-            forwardOrBackwards(1);
-            Thread.sleep(4000);
-            forwardOrBackwards(0);
 
-        }
+
+        motorRightFront.setTargetPosition(300);
+        motorLeftFront.setTargetPosition(300);
+        motorRightBack.setTargetPosition(300);
+        motorLeftBack.setTargetPosition(300);
+
+        motorRightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLeftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorRightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLeftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorRightFront.setPower(.2);
+        motorLeftFront.setPower(.2);
+        motorRightBack.setPower(.2);
+        motorLeftBack.setPower(.2);
+
+        sleep(3000);
 
     }
-    private void forwardOrBackwards(double power){
-        motorRightFront.setPower(power);
-        motorLeftFront.setPower(power);
-        motorRightBack.setPower(power);
-        motorLeftBack.setPower(power);
+    private void TravelDistance(int travel){
+        motorRightFront.setTargetPosition(travel);
+        motorLeftFront.setTargetPosition(travel);
+        motorRightBack.setTargetPosition(travel);
+        motorLeftBack.setTargetPosition(travel);
     }
 
-    private void strafe(double power){
-        motorRightFront.setPower(-power);
-        motorLeftFront.setPower(power);
-        motorRightBack.setPower(-power);
-        motorLeftBack.setPower(power);
+    private void MotorPower(double motor){
+        motorRightFront.setPower(motor);
+        motorLeftFront.setPower(motor);
+        motorRightBack.setPower(motor);
+        motorLeftBack.setPower(motor);
     }
 }
