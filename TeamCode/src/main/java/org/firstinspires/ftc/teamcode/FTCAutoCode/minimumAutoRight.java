75ccab7ff92fbdcd3a9b99540159bb8357b2775e
krish <joshikrish2022@gmail.com>
2024-10-30 18:32:10 -0500

    worst case scenario auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCAutoCode/minimumAutoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCAutoCode/minimumAutoRight.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous
 public class minimumAutoRight extends LinearOpMode {
@@ -13,6 +14,7 @@ public class minimumAutoRight extends LinearOpMode {
         private DcMotor motorRightBack = null;
         private DcMotor motorLeftFront = null;
         private DcMotor motorLeftBack = null;
+        public Servo servo;
 
         @Override
         public void runOpMode() throws InterruptedException {
@@ -21,8 +23,7 @@ public class minimumAutoRight extends LinearOpMode {
             motorRightBack = hardwareMap.get(DcMotor.class, "BR");
             motorLeftBack = hardwareMap.get(DcMotor.class, "BL");
             motorLeftFront = hardwareMap.get(DcMotor.class, "FL");
-
-
+            servo = hardwareMap.get(Servo.class, "servo");
 
             motorRightBack.setDirection(DcMotorSimple.Direction.FORWARD);
             motorRightFront.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -33,15 +34,44 @@ public class minimumAutoRight extends LinearOpMode {
             motorRightFront.setPower(0.3);
             motorLeftBack.setPower(-0.3);
             motorLeftFront.setPower(0.3);
-            sleep(2500);
+
+            sleep(811);
             //strafe right
+
+            servo.setPosition(0);
+            servo.setPosition(0.7);
+            //simple arm
 
             motorRightBack.setPower(-0.3);
             motorRightFront.setPower(-0.3);
             motorLeftBack.setPower(0.3);
             motorLeftFront.setPower(0.3);
-            sleep(1700);
-        //move forward
+            sleep(2000);
+            //move forward
+
+            servo.setPosition(0.3);
+            //simple arm
+
+            motorRightBack.setPower(0.3);
+            motorRightFront.setPower(0.3);
+            motorLeftBack.setPower(-0.3);
+            motorLeftFront.setPower(-0.3);
+            sleep(1500);
+            //backwards
+
+            motorRightBack.setPower(-0.3);
+            motorRightFront.setPower(-0.3);
+            motorLeftBack.setPower(-0.3);
+            motorLeftFront.setPower(-0.3);
+            sleep(1250);
+            //turn
+
+            motorRightBack.setPower(-0.6);
+            motorRightFront.setPower(-0.6);
+            motorLeftBack.setPower(0.6);
+            motorLeftFront.setPower(0.6);
+            sleep(3500);
+            //go forward
 
 
         }
