d75e1637eecd4304f065f798d545034f8b838118
krish <joshikrish2022@gmail.com>
2024-11-28 15:11:45 -0600

    started coding our better auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCAutoCode/sixteenAuto.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.FTCAutoCode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class sixteenAuto extends TemplateAuto {
+
+    public void runOpMode() throws InterruptedException{
+        super.runOpMode();
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        waitForStart();
+
+        arm.setTargetPosition(50);
+        arm.setPower(0.3);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        strafeRight(0.2);
+        sleep(100);
+
+         moveFront(0.3);
+         sleep(500);
+
+         strafeRight(0.4);
+         sleep(600);
+
+         moveFront(0.4);
+         sleep(400);
+
+         rotateLeft(0.5);
+         sleep(900);
+
+         arm.setTargetPosition(750);
+         arm.setPower(0.5);
+         arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+         slides.set
+
+}
+ }
